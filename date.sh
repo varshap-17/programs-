8e0b0dbffe60b9b888024d6da26163034e86b937
varsha p <varshap630@gmail.com>
2022-06-12 17:43:09 +0530

    Day and Month program

---
+++ date.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+date1="0319"
+date2="0621"
+echo "please enter the date in mmdd formate:"
+read x
+if [ $x -gt $date1 -a $x -lt $date2 ]
+then
+	echo "True"
+else
+	echo "False"
+fi
