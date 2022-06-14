31f96d69850837228896ff16cc2bfd7fcad89524
varsha p <varshap630@gmail.com>
2022-06-14 11:18:11 +0530

    Power of 2 range

---
+++ powers2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+echo "enter a number:"
+read number
+exp=2
+result=1
+while [ $result -le 256 ]
+do
+	echo "power table of the $number is:$result"
+	result=$(($result*$exp))
+done
