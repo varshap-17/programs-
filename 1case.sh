854a5264c3afe9b2fd5e06a8fd9b4c9ae3368ce8
varsha p <varshap630@gmail.com>
2022-06-14 10:53:30 +0530

    Single digit in case

---
+++ 1case.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+echo "enter the single digit number:"
+read n
+case "$n" in
+0) echo "zero"
+;;
+1) echo "one"
+;;
+2) echo "two"
+;;
+3) echo "three"
+;;
+4) echo "four"
+;;
+5) echo "five"
+;;
+6) echo "six"
+;;
+7) echo "seven"
+;;
+8) echo "eight"
+;;
+9) echo "nine"
+;;
+*) echo "this is not single digit number"
+;;
+esac
