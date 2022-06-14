6e8ad812f7f572433fc7a61b6e0bfe22422db620
varsha p <varshap630@gmail.com>
2022-06-14 10:55:55 +0530

    Week day in case

---
+++ numtoday.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+
+echo "Enter the single digit number:"
+read n
+case "$n" in
+1) echo "Sunday"
+;;
+2) echo "Monday"
+;;
+3) echo "Tuesday"
+;;
+4) echo "Wednesday"
+;;
+5) echo "Thursday"
+;;
+6) echo "Friday"
+;;
+7) echo "Saturday"
+;;
+*) echo "not a week day"
+;;
+esac
+
